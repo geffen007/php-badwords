4497679879290b70f52b87737d22199d426abca9
Guido <guido.amodio@gmail.com>
2020-09-15 16:53:22 +0200

    1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+// $( document ).ready(function() {
+//
+//     var badword = $('button').click(function(){
+//         var a = prompt('inserisci una parola da sostituire');
+//
+//         $.ajax({
+//             method: "GET",
+//             data: { badword: a }
+//         })
+//
+//     });
+//
+//
+//
+// });
